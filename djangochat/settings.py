aceab97a50a6d23050202155b82cb353d7a84d19
Dhiya Joseph <dhiyajsph@gmail.com>
2024-02-03 12:12:13 +0530

    fourth commit

--- djangochat/settings.py
+++ djangochat/settings.py
@@ -74,7 +74,14 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'djangochat.wsgi.application'
+ASGI_APPLICATION = 'djangochat.asgi.routing.application'
 
+
+CHANNEL_LAYERS = {
+    'default': {
+        'BACKEND': 'channels.layers.InMemoryChannelLayer'
+    }
+}
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
